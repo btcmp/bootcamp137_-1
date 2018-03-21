2705a3314bee5d078e0ce3c1e88a07f5f523b59b
krissunu <kris.sunu@gmail.com>
2018-03-21 10:27:49 +0700

    add EmployeeDao & EmployeeDaoImpl

---
+++ src/main/java/com/xsis/mp1/dao/EmployeeDao.java
@@ -0,0 +1,27 @@
+package com.xsis.mp1.dao;
+
+import java.util.List;
+
+import com.xsis.mp1.model.Employee;
+
+public interface EmployeeDao {
+
+	// create
+	public void save(Employee employee);
+
+	// read
+	// employee -- list
+	public List<Employee> selectAll();
+
+	/// single employee
+	public Employee getOne(Employee employee);
+
+	// update
+	public void update(Employee employee);
+
+	// delete
+	public void delete(Employee employee);
+
+	// save or update
+	public void saveOrUpdate(Employee employee);
+}
---
+++ src/main/java/com/xsis/mp1/dao/EmployeeDaoImpl.java
@@ -0,0 +1,52 @@
+package com.xsis.mp1.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.xsis.mp1.model.Employee;
+
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Autowired
+	SessionFactory sessionFactory;
+
+	public void save(Employee employee) {
+		Session session = sessionFactory.getCurrentSession();
+		session.save(employee);
+		session.flush();
+	}
+
+	public List<Employee> selectAll() {
+		Session session = sessionFactory.getCurrentSession();
+		return session.createCriteria(Employee.class).list();
+	}
+
+	public Employee getOne(Employee employee) {
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(Employee.class, employee.getId());
+	}
+
+	public void update(Employee employee) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(employee);
+		session.flush();
+	}
+
+	public void delete(Employee employee) {
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(employee);
+		;
+		session.flush();
+	}
+
+	public void saveOrUpdate(Employee employee) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(employee);
+		session.flush();
+	}
+}
