0b4274b2b1d577c21c2cc891b051cb095d0e4e27
ACER <ACER@AldyAand>
2018-03-21 13:28:33 +0700

    update varian dan item (dao dan service)

---
+++ src/main/java/com/xsis/mp1/dao/ItemDaoImpl.java
@@ -0,0 +1,53 @@
+package com.xsis.mp1.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.xsis.mp1.model.Employee;
+import com.xsis.mp1.model.Item;
+
+public class ItemDaoImpl implements ItemDao {
+
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	public void save(Item item) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		session.save(item);
+		session.flush();
+	}
+
+	public List<Item> selectAll() {
+		Session session = sessionFactory.getCurrentSession();
+		return session.createCriteria(Item.class).list();
+	}
+
+	public Item getOne(Item item) {
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(Item.class, item.getId());
+	}
+
+	public void update(Item item) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		session.update(item);
+		session.flush();
+	}
+
+	public void delete(Item item) {
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(item);
+		session.flush();
+	}
+
+	public void saveOrUpdate(Item item) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(item);
+		session.flush();
+	}
+
+}
